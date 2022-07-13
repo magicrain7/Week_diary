53e5e49540580e7de5e7f712bd8510ae1253fca9
Hyolim Lim <109011766+hyolimlim@users.noreply.github.com>
2022-07-14 03:05:04 +0900

    완전 단순 게시판..
    윤종님 파일 받아서 진행하니까 그동안 발생하던 오류없이 잘 진행되더라고요ㅠㅠ 
    완성 후 해봤을 때는 loginpage-> indexpage로 잘 넘어갔어요.
    확인 부탁드립니다~
    +static 에는 제거 sample img만 들어가있어요.

--- app.py
+++ app.py
@@ -1,46 +1,145 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.apzpx.mongodb.net/Cluster0?retryWrites=true&w=majority')
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.dd9kdbk.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-# 로그인
-@app.route("/api/login", methods=["POST"])
-def web_mars_post():
-    # name_give를 받아서 name_receive로 저장해라
-    return jsonify({})
 
-#회원가입
-@app.route("/api/signup", methods=["POST"])
-def web_mars_post():
-    # name_give를 받아서 name_receive로 저장해라
-    return jsonify({})
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
 
-#글작성
-@app.route("/api/posts", methods=["POST"])
-def web_mars_post():
-    # name_give를 받아서 name_receive로 저장해라
-    return jsonify({})
 
-#글목록
-@app.route("//api/posts", methods=["GET"])
-def web_mars_get():
-    # db에서 조회할것을 가져와서 변수에 담는다.
-    return jsonify({})
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
 
-#검색
-@app.route("/mars", methods=["GET"])
-def web_mars_get():
-    # db에서 조회할것을 가져와서 변수에 담는다.
-    order_list = list(db.mars.find({},{'_id':False}))
-    return jsonify({'orders': order_list})
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "profile_name": username_receive,
+        "profile_pic": "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png",
+        "profile_info": ""
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    # print(value_receive, type_receive, exists)
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/update_profile', methods=['POST'])
+def save_img():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        name_receive = request.form["name_give"]
+        about_receive = request.form["about_give"]
+        new_doc = {
+            "profile_name": name_receive,
+            "profile_info": about_receive
+        }
+        if 'file_give' in request.files:
+            file = request.files["file_give"]
+            filename = secure_filename(file.filename)
+            extension = filename.split(".")[-1]
+            file_path = f"profile_pics/{username}.{extension}"
+            file.save("./static/"+file_path)
+            new_doc["profile_pic"] = filename
+            new_doc["profile_pic_real"] = file_path
+        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/post', methods=['POST'])
+def save_post():
+    title_receive = request.form['title_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        "title": title_receive,
+        "comment": comment_receive,
+    }
+    db.post.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
+
+@app.route('/post', methods=['GET'])
+def show_post():
+    posts = list(db.post.find({}, {'_id': False}))
+    return jsonify({'all_post': posts})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
